28d2979f7f913d5184960624190de239fe5c0db7
Scott Carr <kscarr73@gmail.com>
2025-06-25 15:12:57 -0500

    Handle cases where subject or convertObj are `null`

--- src/main/java/com/progbits/api/conversion/ApiObjectConverter.java
+++ src/main/java/com/progbits/api/conversion/ApiObjectConverter.java
@@ -3,6 +3,7 @@ package com.progbits.api.conversion;
 import com.progbits.api.exception.ApiClassNotFoundException;
 import com.progbits.api.exception.ApiException;
 import com.progbits.api.model.ApiObject;
+import com.progbits.api.model.ApiObjectUtils;
 import com.progbits.api.parser.JsonObjectParser;
 import com.progbits.api.utils.service.ApiInstance;
 import com.progbits.api.utils.service.ApiService;
@@ -56,20 +57,20 @@ public class ApiObjectConverter implements ApiService {
     public static final String FIELD_DOT_METHOD = ".method";
     public static final String FIELD_TYPE = "type";
     public static final String FIELD_REQUIRED = "required";
-    
+
     private static final String CONTROL_REQUIRED = "$required";
     private static final String FIELD_ROOT = "root";
-    
+
     private final Map<String, ApiMethodHandler> methods = new HashMap<>();
 
     private static final JsonObjectParser jsonParser = new JsonObjectParser(true);
     private static final JsonObjectWriter jsonWriter = new JsonObjectWriter(true);
-    
+
     /*
       Reverse Methods are used when reversing a Conversion Config object
      */
     private final ApiObject reverseMethods = new ApiObject();
-    
+
     public ApiObject getReverseMethods() {
         return reverseMethods;
     }
@@ -85,7 +86,7 @@ public class ApiObjectConverter implements ApiService {
             if (CONTROL_REQUIRED.equals(field)) {
                 continue;
             }
-            
+
             if (convertObj.getType(field) == ApiObject.TYPE_STRING) {
                 respObj.put(convertObj.getString(field), field);
             } else if (convertObj.getType(field) == ApiObject.TYPE_OBJECT) {
@@ -102,15 +103,16 @@ public class ApiObjectConverter implements ApiService {
 
         return respObj;
     }
-    
+
     /**
      * Convert ApiObject with root list or Single using a Conversion Object
-     * 
-     * <p>Defaults includeAll to false</p>
-     * 
+     *
+     * <p>
+     * Defaults includeAll to false</p>
+     *
      * @param subject The subject to perform the conversion on
      * @param convertObj Field Mapping used for Conversion
-     * 
+     *
      * @return The Converted Object
      *
      * @throws ApiException
@@ -118,66 +120,76 @@ public class ApiObjectConverter implements ApiService {
     public ApiObject convertObject(ApiObject subject, ApiObject convertObj) throws ApiException {
         return convertObject(subject, convertObj, false);
     }
-    
+
     /**
      * Convert ApiObject with root list or Single using a Conversion Object
      *
      * @param subject The subject to perform the conversion on
      * @param convertObj Field Mapping used for Conversion
      * @param includeAll true/false Return ALL fields from subject
-     * 
+     *
      * @return The Converted Object
      *
      * @throws ApiException
      */
     public ApiObject convertObject(ApiObject subject, ApiObject convertObj, boolean includeAll) throws ApiException {
-        if (subject.containsKey(FIELD_ROOT)) {
-            ApiObject respObj = new ApiObject();
-            
-            respObj.createList(FIELD_ROOT);
-            
-            for (var entry : subject.getList(FIELD_ROOT)) {
-                respObj.getList(FIELD_ROOT).add(convertSingle(entry, convertObj, includeAll));
+        if (subject != null) {
+            if (convertObj != null) {
+                if (subject.containsKey(FIELD_ROOT)) {
+                    ApiObject respObj = new ApiObject();
+
+                    respObj.createList(FIELD_ROOT);
+
+                    for (var entry : subject.getList(FIELD_ROOT)) {
+                        respObj.getList(FIELD_ROOT).add(convertSingle(entry, convertObj, includeAll));
+                    }
+
+                    return respObj;
+                } else {
+                    return convertSingle(subject, convertObj, includeAll);
+                }
+            } else {
+                log.warn("Convert Object was NULL");
+                
+                return ApiObjectUtils.cloneApiObject(subject, null);
             }
-            
-            return respObj;
         } else {
-            return convertSingle(subject, convertObj, includeAll);
+            return null;
         }
     }
-    
+
     /**
      * Convert a single ApiObject using a Conversion Object
      *
      * @param subject The subject to perform the conversion on
      * @param convertObj Field Mapping used for Conversion
      * @param includeAll true/false Return ALL fields from subject
-     * 
+     *
      * @return The Converted Object
      *
      * @throws ApiException
      */
-    public ApiObject convertSingle(ApiObject subject, ApiObject convertObj, boolean includeAll) throws ApiException {
+    private ApiObject convertSingle(ApiObject subject, ApiObject convertObj, boolean includeAll) throws ApiException {
         ApiObject respObj = new ApiObject();
 
         if (convertObj.getType(CONTROL_REQUIRED) == ApiObject.TYPE_STRINGARRAY) {
             StringBuilder sbErr = new StringBuilder();
-            
+
             for (String entry : convertObj.getStringArray(CONTROL_REQUIRED)) {
                 if (!subject.isSet(entry)) {
                     if (!sbErr.isEmpty()) {
                         sbErr.append("\n");
                     }
-                    
+
                     sbErr.append(entry).append(" IS REQUIRED");
                 }
             }
-            
+
             if (!sbErr.isEmpty()) {
                 throw new ApiException(400, sbErr.toString());
             }
         }
-        
+
         for (String subjectFld : subject.keySet()) {
             if (convertObj.getType(subjectFld) == ApiObject.TYPE_STRING) {
                 processField(respObj, convertObj.getString(subjectFld), subject.get(subjectFld));
@@ -194,27 +206,27 @@ public class ApiObjectConverter implements ApiService {
             } else if (includeAll) {
                 if ("orderBy".equals(subjectFld)) {
                     respObj.createStringArray(subjectFld);
-                    
+
                     for (var orderEntry : subject.getStringArray("orderBy")) {
                         boolean minusExists = false;
                         String lclOrderEntry = orderEntry;
                         String replaceEntry;
-                        
+
                         if (orderEntry.startsWith("-")) {
-                            lclOrderEntry = orderEntry.substring(1);   
+                            lclOrderEntry = orderEntry.substring(1);
                             minusExists = true;
                         }
-                        
+
                         if (convertObj.getType(lclOrderEntry) == ApiObject.TYPE_OBJECT) {
                             replaceEntry = convertObj.getString(lclOrderEntry + FIELD_DOT_FIELD);
                         } else {
                             replaceEntry = convertObj.getString(lclOrderEntry);
                         }
-                        
+
                         if (minusExists) {
                             replaceEntry = "-" + replaceEntry;
                         }
-                        
+
                         respObj.getStringArray(subjectFld).add(replaceEntry);
                     }
                 } else {
@@ -225,73 +237,82 @@ public class ApiObjectConverter implements ApiService {
 
         return respObj;
     }
-    
+
     /**
      * Convert ApiObject with root list or Single using a Conversion Object
      *
-     * <p>NOTE: Iterates over convertObj instead of subject</p>
-     * 
+     * <p>
+     * NOTE: Iterates over convertObj instead of subject</p>
+     *
      * @param subject The subject to perform the conversion on
      * @param convertObj Field Mapping used for Conversion
      * @param includeAll true/false Return ALL fields from subject
-     * 
+     *
      * @return The Converted Object
      *
      * @throws ApiException
      */
     public ApiObject convertObjectV2(ApiObject subject, ApiObject convertObj, boolean includeAll) throws ApiException {
-        if (subject.containsKey(FIELD_ROOT)) {
-            ApiObject respObj = new ApiObject();
-            
-            respObj.createList(FIELD_ROOT);
-            
-            for (var entry : subject.getList(FIELD_ROOT)) {
-                respObj.getList(FIELD_ROOT).add(convertSingleV2(entry, convertObj, includeAll));
+        if (subject != null) {
+            if (convertObj != null) {
+                if (subject.containsKey(FIELD_ROOT)) {
+                    ApiObject respObj = new ApiObject();
+
+                    respObj.createList(FIELD_ROOT);
+
+                    for (var entry : subject.getList(FIELD_ROOT)) {
+                        respObj.getList(FIELD_ROOT).add(convertSingleV2(entry, convertObj, includeAll));
+                    }
+
+                    return respObj;
+                } else {
+                    return convertSingleV2(subject, convertObj, includeAll);
+                }
+            } else {
+                return ApiObjectUtils.cloneApiObject(subject, null);
             }
-            
-            return respObj;
         } else {
-            return convertSingleV2(subject, convertObj, includeAll);
+            return null;
         }
     }
-    
+
     /**
      * Convert a single ApiObject using a Conversion Object
      *
      * @param subject The subject to perform the conversion on
      * @param convertObj Field Mapping used for Conversion
      * @param includeAll true/false Return ALL fields from subject
-     * 
+     *
      * @return The Converted Object
      *
      * @throws ApiException
      */
     private ApiObject convertSingleV2(ApiObject subject, ApiObject convertObj, boolean includeAll) throws ApiException {
         ApiObject respObj = new ApiObject();
-        
+
         for (String convertFld : convertObj.keySet()) {
             if (convertObj.getType(convertFld) == ApiObject.TYPE_STRING) {
                 processField(respObj, convertObj.getString(convertFld), subject.get(convertFld));
             } else if (convertObj.getType(convertFld) == ApiObject.TYPE_OBJECT) {
                 ApiObject convert = convertObj.getObject(convertFld);
                 String intFld = convertFld; // Internal Field for use if # is present
-                
+
                 if (intFld.contains("#")) {
                     intFld = intFld.substring(0, intFld.indexOf("#"));
                 }
-                
+
                 var toMethod = convert.getString(FIELD_DOT_METHOD);
-                
+
                 if (convert.isSet("default") && subject.getType(intFld) == ApiObject.TYPE_NULL) {
                     subject.put(intFld, convert.get("default"));
                 }
-                
+
                 if (convert.isSet("required") && subject.isNull(intFld)) {
                     throw new ApiException(415, intFld + " IS Required");
                 }
-                
+
                 if (convert.isSet(FIELD_TYPE) && !validateFieldType(intFld, convert.getString(FIELD_TYPE), subject)) {
-                    throw new ApiException(415, intFld + " Expected Type " + convert.getString(FIELD_TYPE) );
+                    throw new ApiException(415, intFld + " Expected Type " + convert.getString(FIELD_TYPE));
                 }
 
                 if (toMethod != null) {
@@ -303,13 +324,13 @@ public class ApiObjectConverter implements ApiService {
                 } else {
                     if (convert.getString(FIELD_FIELD).contains(".")) {
                         String[] fldSplit = convert.getString(FIELD_FIELD).split("\\.");
-                        
+
                         ApiObject objSub = respObj.getObject(fldSplit[0]);
-                        
+
                         if (objSub == null) {
                             objSub = respObj.createObject(fldSplit[0]);
                         }
-                        
+
                         objSub.put(fldSplit[1], subject.get(intFld));
                     } else {
                         respObj.put(convert.getString(FIELD_FIELD), subject.get(intFld));
@@ -318,27 +339,27 @@ public class ApiObjectConverter implements ApiService {
             } else if (includeAll) {
                 if ("orderBy".equals(convertFld)) {
                     respObj.createStringArray(convertFld);
-                    
+
                     for (var orderEntry : subject.getStringArray("orderBy")) {
                         boolean minusExists = false;
                         String lclOrderEntry = orderEntry;
                         String replaceEntry;
-                        
+
                         if (orderEntry.startsWith("-")) {
-                            lclOrderEntry = orderEntry.substring(1);   
+                            lclOrderEntry = orderEntry.substring(1);
                             minusExists = true;
                         }
-                        
+
                         if (convertObj.getType(lclOrderEntry) == ApiObject.TYPE_OBJECT) {
                             replaceEntry = convertObj.getString(lclOrderEntry + FIELD_DOT_FIELD);
                         } else {
                             replaceEntry = convertObj.getString(lclOrderEntry);
                         }
-                        
+
                         if (minusExists) {
                             replaceEntry = "-" + replaceEntry;
                         }
-                        
+
                         respObj.getStringArray(convertFld).add(replaceEntry);
                     }
                 } else {
@@ -352,26 +373,34 @@ public class ApiObjectConverter implements ApiService {
 
     private boolean validateFieldType(String fld, String typeString, ApiObject subject) {
         return switch (typeString) {
-            case "string" -> subject.getType(fld) == ApiObject.TYPE_STRING;
-            case "integer" -> subject.getType(fld) == ApiObject.TYPE_INTEGER;
-            case "long" -> subject.getType(fld) == ApiObject.TYPE_LONG;
-            case "object" -> subject.getType(fld) == ApiObject.TYPE_OBJECT;
-            case "boolean" -> subject.getType(fld) == ApiObject.TYPE_BOOLEAN;
-            case "arraylist" -> subject.getType(fld) == ApiObject.TYPE_ARRAYLIST;
-            case "stringarray" -> subject.getType(fld) == ApiObject.TYPE_STRINGARRAY;
-                
-            default -> false;
+            case "string" ->
+                subject.getType(fld) == ApiObject.TYPE_STRING;
+            case "integer" ->
+                subject.getType(fld) == ApiObject.TYPE_INTEGER;
+            case "long" ->
+                subject.getType(fld) == ApiObject.TYPE_LONG;
+            case "object" ->
+                subject.getType(fld) == ApiObject.TYPE_OBJECT;
+            case "boolean" ->
+                subject.getType(fld) == ApiObject.TYPE_BOOLEAN;
+            case "arraylist" ->
+                subject.getType(fld) == ApiObject.TYPE_ARRAYLIST;
+            case "stringarray" ->
+                subject.getType(fld) == ApiObject.TYPE_STRINGARRAY;
+
+            default ->
+                false;
         };
     }
-    
+
     private void processField(ApiObject to, String fieldName, Object value) {
         if (fieldName.contains(".")) {
             String[] sName = fieldName.split("\\.");
-            
+
             if (to.getType(sName[0]) != ApiObject.TYPE_OBJECT) {
                 to.createObject(sName[0]);
             }
-            
+
             to.getObject(sName[0]).put(sName[1], value);
         } else {
             to.put(fieldName, value);
@@ -386,7 +415,7 @@ public class ApiObjectConverter implements ApiService {
             return null;
         }
     }
-    
+
     private ApiObject parseJsonToObject(String subject) {
         try {
             return jsonParser.parseSingle(new StringReader(subject));
@@ -395,7 +424,7 @@ public class ApiObjectConverter implements ApiService {
             return null;
         }
     }
-    
+
     /**
      * Convert Object to JSON String
      *
@@ -414,7 +443,6 @@ public class ApiObjectConverter implements ApiService {
         }
     }
 
-    
     /**
      * Convert JSON String to Object
      *
@@ -445,7 +473,7 @@ public class ApiObjectConverter implements ApiService {
 
                 processField(to, processObj.getString(FIELD_FIELD), bTest);
             }
-            
+
             case ApiObject.TYPE_STRING -> {
                 if ("true".equals(from.getString(fieldFrom))) {
                     processField(to, processObj.getString(FIELD_FIELD), true);
@@ -453,8 +481,9 @@ public class ApiObjectConverter implements ApiService {
                     processField(to, processObj.getString(FIELD_FIELD), false);
                 }
             }
-            
-            default -> {}
+
+            default -> {
+            }
         }
     }
 
@@ -463,26 +492,26 @@ public class ApiObjectConverter implements ApiService {
             processField(to, processObj.getString(FIELD_FIELD), from.isSet(fieldFrom) ? 1 : 0);
         }
     }
-    
+
     private void convertStringToDate(ApiObject from, ApiObject to, String fieldFrom, ApiObject processObj) throws ApiException {
         if (from.getType(fieldFrom) == ApiObject.TYPE_STRING) {
             String strFrom = from.getString(fieldFrom);
-            
+
             if (strFrom.length() == 10) {
                 // Expected format 2025-05-02 yyyy-MM-dd add rest of string
                 strFrom += "T00:00:00.000Z";
             }
-            
+
             try {
                 OffsetDateTime dteFrom = OffsetDateTime.parse(strFrom);
-                
+
                 processField(to, processObj.getString(FIELD_FIELD), dteFrom);
             } catch (DateTimeParseException dtp) {
                 throw new ApiException(410, "Field (" + fieldFrom + ") Cannot Be Parsed to DateTime. Expected Format: yyyy-MM-ddTHH:mm:ss.SSSX");
             }
         }
     }
-    
+
     private void convertDateToString(ApiObject from, ApiObject to, String fieldFrom, ApiObject processObj) throws ApiException {
         if (from.getType(fieldFrom) == ApiObject.TYPE_DATETIME) {
             processField(to, processObj.getString(FIELD_FIELD), from.getDateTime(fieldFrom).format(DateTimeFormatter.ISO_DATE_TIME));
